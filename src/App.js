b070c299e29275cb792fccb39c3e83478140ae0f
Brennan Fife <brenfife@Brennans-MacBook-Pro.local>
2019-09-24 12:31:38 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-        </div>
+        <Navbar />
       </BrowserRouter>
     );
   }
